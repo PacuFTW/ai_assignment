a1eaa2b8c08f19184bb04b36a518f8cd89ee2d34
Karakai Patrik <patrikkarakai@gmail.com>
2023-11-07 22:22:28 +0100

    init

---
+++ graph.py
@@ -0,0 +1,31 @@
+def dfs(graph, start):
+    visited = set()
+    finished = set()
+
+    def visit(node):
+        print(f"Visited: {node}")
+        visited.add(node)
+
+    def finish(node):
+        print(f"Finished: {node}")
+        finished.add(node)
+
+    def dfs_recursive(node):
+        visit(node)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs_recursive(neighbor)
+        finish(node)
+
+    dfs_recursive(start)
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+dfs(graph, 'A')
